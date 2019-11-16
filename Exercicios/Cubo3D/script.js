00caf8cf1aa3528b3ca59b38ce983a270ea70bc6
tomasferranti <tomas_ferranti_07@hotmail.com>
2019-11-16 18:50:05 -0300

    atualizacoes

--- Exercicios/Cubo3D/script.js
+++ Exercicios/Cubo3D/script.js
@@ -1,92 +1,66 @@
+//Algumas variáveis globais
 var gl;
 var shaderProgram;
 var cuboVertexPositionBuffer;
 var cuboVertexColorBuffer;
 var cuboVertexIndexBuffer;
+var rCubo = 0;
+var ultimo = 0;
 
-
+//Matrizes de transformação usando um sistema de pilha
 var mMatrix = mat4.create();
 var mMatrixPilha = [];
 var pMatrix = mat4.create();
 var vMatrix = mat4.create();
 
-var rCubo = 0;
-
-var ultimo = 0;
-
-// Iniciar o ambiente quando a página for carregada
+//Iniciar o ambiente quando a página for carregada
 $(function(){
 	iniciaWebGL();
 });
 
-// Iniciar o ambiente
+//Iniciar o nosso canvas com webGl3D
 function iniciaWebGL(){
-	var canvas = $('#licao01-canvas')[0];
-	iniciarGL(canvas); // Definir como um canvas 3D
-	iniciarShaders();  // Obter e processar os Shaders
-	iniciarBuffers();  // Enviar o triângulo e quadrado na GPU
-	iniciarAmbiente(); // Definir background e cor do objeto
-	tick();
+	var canvas = $('#canvasWebGl3D')[0]; // Pegando seu id
+	iniciarGL(canvas); 					 // Definir o canvas com webgl 3D
+	iniciarShaders();  					 // Obter e processar os Shaders
+	iniciarBuffers();  					 // Enviar o cubo para a GPU
+	iniciarAmbiente(); 					 // Definir background e cor do objeto
+	tick();            					 // Desenho dos objetos
 }
 
-function tick(){
-	requestAnimFrame(tick);
-	desenharCena();
-	animar();
-}
-
+//Iniciando o WebGL 3D
 function iniciarGL(canvas){
 	try{
 		gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+		//Definindo a nossa viewPort como o tamanho do canvas
 		gl.viewportWidth = canvas.width;
 		gl.viewportHeight = canvas.height;
 	}catch(e){
+		//Se não inicializou retorne um aviso
 		if(!gl){
 			alert("Não pode inicializar WebGL, desculpe");
 		}			
 	}
 }
 
-function iniciarShaders(){
-	var vertexShader = getShader(gl, "#shader-vs");
-	var fragmentShader = getShader(gl, "#shader-fs");
-
-	shaderProgram = gl.createProgram();
-	gl.attachShader(shaderProgram, vertexShader);
-	gl.attachShader(shaderProgram, fragmentShader);
-	gl.linkProgram(shaderProgram);
-
-	if(!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)){
-		alert("Não pode inicializar shaders");
-	}
-
-	gl.useProgram(shaderProgram);
-
-	shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
-	gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
-
-	shaderProgram.vertexColorAttribute = gl.getAttribLocation(shaderProgram, "aVertexColor");
-	gl.enableVertexAttribArray(shaderProgram.vertexColorAttribute);
-
-	shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
-	shaderProgram.vMatrixUniform = gl.getUniformLocation(shaderProgram, "uVMatrix");
-	shaderProgram.mMatrixUniform = gl.getUniformLocation(shaderProgram, "uMMatrix");
-}
-
+//Função que pega o shader dado o id do script
 function getShader(gl, id){
+	//Pegando nosso script
 	var shaderScript = $(id)[0];
 	if(!shaderScript){
 		return null;
 	}
-
-	var str = "";
+	
+	//Identificando sua origem
+	var shaderSourceStr = "";
 	var k = shaderScript.firstChild;
 	while(k){
 		if(k.nodeType == 3)
-		str += k.textContent;
+		shaderSourceStr += k.textContent;
 		k = k.nextSibling;
 	}
-
+	
+	//Criando seu shader
 	var shader;
 	if(shaderScript.type == "x-shader/x-fragment"){
 		shader = gl.createShader(gl.FRAGMENT_SHADER);
@@ -95,10 +69,12 @@ function getShader(gl, id){
 	}else{
 		return null;
 	}
-
-	gl.shaderSource(shader, str);
+	
+	//Dizendo sua origem e compilando
+	gl.shaderSource(shader, shaderSourceStr);
 	gl.compileShader(shader);
 
+	//Se não compilou retorne um aviso
 	if(!gl.getShaderParameter(shader, gl.COMPILE_STATUS)){
 		alert(gl.getShaderInfoLog(shader));
 		return null;
@@ -107,7 +83,41 @@ function getShader(gl, id){
 	return shader;
 }
 
-function iniciarBuffers(){	
+function iniciarShaders(){
+	//Pegando shaders dos dois scripts vS e fS
+	var vertexShader = getShader(gl, "#shader-vs");
+	var fragmentShader = getShader(gl, "#shader-fs");
+
+	//Criando um programa para linkar com o vS (vertex shader) e fS(fragmentSHader)
+	//Esse programar irá lidar com a entrada para os shaders
+	shaderProgram = gl.createProgram();
+	gl.attachShader(shaderProgram, vertexShader);
+	gl.attachShader(shaderProgram, fragmentShader);
+	gl.linkProgram(shaderProgram);
+
+	//Se não conseguiu inicializar
+	if(!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)){
+		alert("Não pode inicializar shaders");
+	}
+
+	//Usando o programa
+	gl.useProgram(shaderProgram);
+	
+	//Estabelecendo atributos do programa como:
+	//Posição
+	shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+	gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+	//Cores
+	shaderProgram.vertexColorAttribute = gl.getAttribLocation(shaderProgram, "aVertexColor");
+	gl.enableVertexAttribArray(shaderProgram.vertexColorAttribute);
+	//Matrizes de transformação
+	shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
+	shaderProgram.vMatrixUniform = gl.getUniformLocation(shaderProgram, "uVMatrix");
+	shaderProgram.mMatrixUniform = gl.getUniformLocation(shaderProgram, "uMMatrix");
+}
+
+function iniciarBuffers(){
+	//Carregando os vertices do cubo e linkando com o ARRAY_BUFFER
 	cuboVertexPositionBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, cuboVertexPositionBuffer);
 	vertices = [
@@ -151,27 +161,22 @@ function iniciarBuffers(){
 	cuboVertexPositionBuffer.itemSize = 3;
 	cuboVertexPositionBuffer.numItems = 4;
 
+	//Carregando as cores do cubo e linkando com o ARRAY_BUFFER
 	cuboVertexColorBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, cuboVertexColorBuffer);
-	cores = [
-	[1.0, 0.0, 0.0, 1.0],     // Frente
-	[1.0, 1.0, 0.0, 1.0],     // Trás
-	[0.0, 1.0, 0.0, 1.0],     // Topo
-	[1.0, 0.5, 0.5, 1.0],     // Base
-	[1.0, 0.0, 1.0, 1.0],     // Direita
-	[0.0, 0.0, 1.0, 1.0]      // Esquerda
-	];
+	//Criando um array com 24*4 valores que indica as cores do cubo
+	//No nosso caso, todas faces são pretas
+	cores = [0.0, 0.0, 0.0, 1.0];
 	var coresReplicadas = [];
-	for (var i in cores) {
-		var cor = cores[i];
-		for (var j=0; j < 4; j++) {
-			coresReplicadas = coresReplicadas.concat(cor);
-		}
+	for (var j=0; j < 24; j++) {
+		coresReplicadas = coresReplicadas.concat(cores);
 	}
+	
 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(coresReplicadas), gl.STATIC_DRAW);
 	cuboVertexColorBuffer.itemSize = 4;
 	cuboVertexColorBuffer.numItems = 24;
 
+	//Carregando o index dos vértices do cubo com as respectivas cores
 	cuboVertexIndexBuffer = gl.createBuffer();
 	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cuboVertexIndexBuffer);
 	var indices = [
@@ -187,40 +192,61 @@ function iniciarBuffers(){
 	cuboVertexIndexBuffer.numItems = 36;
 }
 
+//Iniciando o ambiente 3D
 function iniciarAmbiente(){
-	gl.clearColor(0.0, 0.0, 0.0, 1.0);
+	//Limpa a tela com uma cor específica, no nosso caso cinza
+	gl.clearColor(0.8, 0.8, 0.8, 1.0);
+	//DEPTH_TEST seria o de testar qual objeto está na frente um do outro
+	//para não realizar desenhos desnecessários
 	gl.enable(gl.DEPTH_TEST);
 }
 
+//Desenhando os nossos elementos
+function tick(){
+	//Função específica para próxima repintura: ela mesma
+	requestAnimFrame(tick);
+	//Desenho os objetos
+	desenharCena();
+	//Calculo os valores de animação
+	animar();
+}
+
+function setMatrixUniforms(){
+	//Atribuindo as matrizes de transformação
+	gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
+	gl.uniformMatrix4fv(shaderProgram.vMatrixUniform, false, vMatrix);
+	gl.uniformMatrix4fv(shaderProgram.mMatrixUniform, false, mMatrix);
+}
+
 function desenharCena(){
+	//Limpando os buffers
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+	
+	//Criando as matrizes de transformação
+	//Configuração da câmera: perspective(out, fovy, aspect, near, far)
 	mat4.perspective(45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, pMatrix);
+	//Tornando as duas matrizes identidade
 	mat4.identity(mMatrix);
 	mat4.identity(vMatrix);
 
-	// Desenhando o Quadrado
-	mat4.translate(mMatrix, [0, 0.0, -7]);
-
-	mPushMatrix();
+	//Desenhando o Cubo	
+	mat4.translate(mMatrix, [0, 0, -5]);
 	mat4.rotate(mMatrix, degToRad(rCubo),[1,1,1]);
-
+	mPushMatrix();
+	
+	//Atribuindo os buffers
 	gl.bindBuffer(gl.ARRAY_BUFFER, cuboVertexPositionBuffer);
 	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, cuboVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
 	gl.bindBuffer(gl.ARRAY_BUFFER, cuboVertexColorBuffer);
 	gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, cuboVertexColorBuffer.itemSize, gl.FLOAT, false, 0, 0);
 	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cuboVertexIndexBuffer);
 	setMatrixUniforms();
+	//Desenhando os elementos
 	gl.drawElements(gl.TRIANGLES, cuboVertexIndexBuffer.numItems, gl.UNSIGNED_SHORT,0);
 	mPopMatrix();
 }
 
-function setMatrixUniforms(){
-	gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
-	gl.uniformMatrix4fv(shaderProgram.vMatrixUniform, false, vMatrix);
-	gl.uniformMatrix4fv(shaderProgram.mMatrixUniform, false, mMatrix);
-}
-
-// o 'var ultimo = 0' está no topo (todos as vars globais estão juntas)
+//Simplesmente um método de euler para atualizar os valores da matriz de rotação
 function animar(){
 	var agora = new Date().getTime();
 	if(ultimo != 0){
@@ -230,12 +256,14 @@ function animar(){
 	ultimo = agora;
 }
 
+//mMatrixPilha recebendo matriz
 function mPushMatrix(){
 	var copy = mat4.create();
 	mat4.set(mMatrix, copy);
 	mMatrixPilha.push(copy);
 }
 
+//mMatrixPilha devolvendo matriz
 function mPopMatrix(){
 	if(mMatrixPilha.length == 0){
 		throw "inválido popMatrix!";
@@ -243,6 +271,7 @@ function mPopMatrix(){
 	mMatrix = mMatrixPilha.pop();
 }
 
+//Função simples que transforma de graus para radianos
 function degToRad(graus){
 	return graus * Math.PI / 180;
 }
